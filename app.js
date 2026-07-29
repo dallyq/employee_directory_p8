3f4d67f89f81ff312fb52653cee375a425799980
Dallas Quin <166860264+dallyq@users.noreply.github.com>
2026-07-29 20:12:15 +0200

    Added all code from the guide.

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ function displayEmployees(employeeData) {
     });
 
     gridContainer.innerHTML = employeeHTML;
-    
+
 }
 
 
@@ -75,3 +75,21 @@ function displayModal(index) {
     modalContainer.innerHTML = modalHTML;
 
 }
+
+// Event listeners
+gridContainer.addEventListener('click', e => {
+
+    // make sure the click is not on the gridContainer itself
+    if ( e.target !== gridContainer ) {
+
+        // select the card element based on its proximity to actual element clicked
+        const card = e.target.closest(".card");
+        const index = card.getAttribute('data-index');
+
+        displayModal(index);
+    }
+});
+
+modalClose.addEventListener('click', () => {
+    overlay.classList.add("hidden");
+});
