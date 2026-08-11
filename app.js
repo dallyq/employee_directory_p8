51e2422c425359b47520d0eaa015384143fe193f
Dallas Quin <166860264+dallyq@users.noreply.github.com>
2026-08-11 16:18:54 +0200

    Added state abbreviations to the code.

--- app.js
+++ app.js
@@ -6,6 +6,21 @@ const gridContainer = document.querySelector(".grid-container");
 const overlay = document.querySelector(".overlay");
 const modalContainer = document.querySelector(".modal-content");
 const modalClose = document.querySelector(".modal-close");
+const stateAbbreviations = {
+    "Alabama": "AL", "Alaska": "AK", "Arizona": "AZ", "Arkansas": "AR",
+    "California": "CA", "Colorado": "CO", "Connecticut": "CT", "Delaware": "DE",
+    "Florida": "FL", "Georgia": "GA", "Hawaii": "HI", "Idaho": "ID",
+    "Illinois": "IL", "Indiana": "IN", "Iowa": "IA", "Kansas": "KS",
+    "Kentucky": "KY", "Louisiana": "LA", "Maine": "ME", "Maryland": "MD",
+    "Massachusetts": "MA", "Michigan": "MI", "Minnesota": "MN", "Mississippi": "MS",
+    "Missouri": "MO", "Montana": "MT", "Nebraska": "NE", "Nevada": "NV",
+    "New Hampshire": "NH", "New Jersey": "NJ", "New Mexico": "NM", "New York": "NY",
+    "North Carolina": "NC", "North Dakota": "ND", "Ohio": "OH", "Oklahoma": "OK",
+    "Oregon": "OR", "Pennsylvania": "PA", "Rhode Island": "RI", "South Carolina": "SC",
+    "South Dakota": "SD", "Tennessee": "TN", "Texas": "TX", "Utah": "UT",
+    "Vermont": "VT", "Virginia": "VA", "Washington": "WA", "West Virginia": "WV",
+    "Wisconsin": "WI", "Wyoming": "WY", "District of Columbia": "DC"
+};
 
 // fetch data from API
 fetch(urlAPI)
@@ -56,6 +71,7 @@ function displayModal(index) {
     postcode}, picture } = employees[index];
 
     let date = new Date(dob.date);
+    let stateAbbr = stateAbbreviations[state];
 
     const modalHTML = `
         <img class="avatar" src="${picture.large}" />
@@ -65,7 +81,7 @@ function displayModal(index) {
             <p class="address">${city}</p>
             <hr />
             <p>${phone}</p>
-            <p class="address">${street.number} ${street.name}, ${state} ${postcode}</p>
+            <p class="address">${street.number} ${street.name} ${state} ${stateAbbr}, ${postcode}</p>
             <p>Birthday: 
             ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
         </div>
